e90a7be66af631442464c50d4fe1c2374c810fd4
Nuno Henrique Ferreira <nunohenriqueferreira@Mac.lan>
2025-12-20 15:29:05 +0000

    addition of sys.argv to the main file

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 from stats import get_num_words, get_count_characters, print_report
+import sys
 
 def main():
-    print("-------- Word Count --------")
-    print(get_num_words("books/frankenstein.txt"))
-    print("----- Character Count -----")
-    result = get_count_characters("books/frankenstein.txt")
-    print_report(result)
-    print("============ END ============")
+    if len(sys.argv) != 2: #A list of strings representing the arguments passed to the script. 
+        print("Usage: python3 main.py <path_to_book>") #By writing this command on the terminal(<path_to_book> = books/text_of_user_choosing)
+        sys.exit(1) #Exits the script with a status code of 1. Used for validation of solution
+    else:
+        print("-------- Word Count --------")
+        print(get_num_words(sys.argv[1])) #We call the second entry in sys.argv, after the terminal command
+        print("----- Character Count -----")
+        result = get_count_characters(sys.argv[1])
+        print_report(result)
+        print("============ END ============")
 
 main()
+
+#Removing hard coded book paths. Example of Hard coded: books/frankenstein.text
